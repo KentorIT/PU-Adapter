204d1258f9eb8a618661a33e9eddb288602dfb22
Albin Sunnanbo <albin.sunnanbo@kentor.se>
2015-09-16 22:11:43 +0200

    Fetcher and command line client

---
+++ PU-Adapter/PU-Adapter.CommandLine/Program.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kentor.PU_Adapter.CommandLine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fetcher = new PU_Adapter.PknodFetcher();
+            while (true)
+            {
+                Console.WriteLine("Enter person number");
+                var input = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(input))
+                {
+                    return;
+                }
+                var result = fetcher.FetchPkNodplusString(input);
+                Console.WriteLine(result);
+                Console.WriteLine("----------------------------------------------");
+                var parsedData = new PknodPlusData(result);
+                var json = JsonConvert.SerializeObject(parsedData, Formatting.Indented);
+                Console.WriteLine(json);
+                Console.WriteLine("----------------------------------------------");
+            }
+        }
+    }
+}
---
+++ PU-Adapter/PknodFetcher.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+
+namespace Kentor.PU_Adapter
+{
+    public class PknodFetcher
+    {
+        public string Password { get; private set; }
+        public Uri PknodUrl { get; private set; }
+        public string UserName { get; private set; }
+        public PknodFetcher()
+        {
+            this.PknodUrl = new Uri(Properties.Settings.Default.PknodUrl);
+            this.UserName = Properties.Settings.Default.UserName;
+            this.Password = Properties.Settings.Default.Password;
+        }
+
+        public string FetchPkNodplusString(string personNumber)
+        {
+            WebClient wc = new WebClient();
+            if (!string.IsNullOrEmpty(UserName))
+            {
+                wc.Credentials = new NetworkCredential(UserName, Password);
+            }
+            var data = wc.DownloadData(new Uri(PknodUrl, "PKNODPLUS?arg=" + Uri.EscapeDataString(personNumber)));
+            var latin1 = Encoding.GetEncoding("ISO-8859-1");
+            var allDataAsText = latin1.GetString(data);
+            var allDataAsRows = allDataAsText.Split(new[] { Environment.NewLine }, StringSplitOptions.None);
+            return allDataAsRows.LastOrDefault();
+        }
+    }
+}
