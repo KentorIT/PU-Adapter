95646cd375d8ed16c0cf2c96f35dfc9f8d856a68
Albin Sunnanbo <albin.sunnanbo@kentor.se>
2015-09-16 22:24:04 +0200

    Must split at all kind of line endings

--- PU-Adapter/PknodFetcher.cs
+++ PU-Adapter/PknodFetcher.cs
@@ -27,9 +27,18 @@ namespace Kentor.PU_Adapter
             }
             var data = wc.DownloadData(new Uri(PknodUrl, "PKNODPLUS?arg=" + Uri.EscapeDataString(personNumber)));
             var latin1 = Encoding.GetEncoding("ISO-8859-1");
+
+            // Data is in the format
+            // -----------------------------------------------------
+            //0
+            //0
+            //704
+            //07040000191212121212191212121912121212121TOLVANSSON, TOLVAN TOLVAR STIGEN              12345STOCKHOLM                           00000000000000000000    0000018019200244  200801162008011600000000                                                                                                                                                                                    00000000000000000000        132204  03132204  V[STRA KUNGSHOLMEN            17101648M22V[STRA KUNGSHOLMEN                                STOCKHOLM / EKER\     1734    CENTRALA STOCKHOLMS PSYKIATRIS1329999                                               8                                                              _
+            // -----------------------------------------------------
+            // (between the dashed lines). We need to pick the first long line. Lets say long as > 100 characters
             var allDataAsText = latin1.GetString(data);
-            var allDataAsRows = allDataAsText.Split(new[] { Environment.NewLine }, StringSplitOptions.None);
-            return allDataAsRows.LastOrDefault();
+            var allDataAsRows = allDataAsText.Split(new[] { Environment.NewLine, "\r", "\n" }, StringSplitOptions.RemoveEmptyEntries);
+            return allDataAsRows.Where(l => l.Length >= 100).FirstOrDefault();
         }
     }
 }
