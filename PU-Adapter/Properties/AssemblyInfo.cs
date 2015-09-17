732c9b33188d73f2f50b680568d0a77ea3d6b7d3
Albin Sunnanbo <albin@sunnanbo.com>
2015-09-17 13:53:43 +0200

    Nuget release script

--- PU-Adapter/Properties/AssemblyInfo.cs
+++ PU-Adapter/Properties/AssemblyInfo.cs
@@ -8,9 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("PU-Adapter")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("PU-Adapter")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -21,16 +19,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("b9228b32-5ed6-48d4-8edb-93666a8336e0")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ VersionInfo.cs
@@ -0,0 +1,18 @@
+using System.Reflection;
+
+[assembly: AssemblyCompany("Kentor")]
+[assembly: AssemblyCopyright("Copyright © Kentor and contributors 2015")]
+
+
+// Kentor.AuthServices uses semantic versioning in three parts
+//
+//      Major Version
+//      Minor Version 
+//      Patch Number
+//
+// An odd patch number is a development version, an even patch number is
+// a relased version.
+
+[assembly: AssemblyVersion("0.0.1")]
+[assembly: AssemblyFileVersion("0.0.1")]
+[assembly: AssemblyInformationalVersion("0.0.1")]
