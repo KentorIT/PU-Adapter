3b6a78f499b4fae74c6af62597111733f6b628f9
Albin Sunnanbo <albin@sunnanbo.com>
2015-09-21 14:38:41 +0200

    Updated version number to 0.0.8 for release.

--- VersionInfo.cs
+++ VersionInfo.cs
@@ -13,6 +13,6 @@ using System.Reflection;
 // An odd patch number is a development version, an even patch number is
 // a relased version.
 
-[assembly: AssemblyVersion("0.0.7")]
-[assembly: AssemblyFileVersion("0.0.7")]
-[assembly: AssemblyInformationalVersion("0.0.7")]
+[assembly: AssemblyVersion("0.0.8")]
+[assembly: AssemblyFileVersion("0.0.8")]
+[assembly: AssemblyInformationalVersion("0.0.8")]
