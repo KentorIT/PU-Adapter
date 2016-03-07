396bc4de7ea85dde27d9fd2c17d5f5adbc970839
Albin Sunnanbo <albin.sunnanbo@kentor.se>
2016-03-07 10:27:42 +0100

    Updated version number to 0.0.15 for development.

--- VersionInfo.cs
+++ VersionInfo.cs
@@ -13,6 +13,6 @@ using System.Reflection;
 // An odd patch number is a development version, an even patch number is
 // a relased version.
 
-[assembly: AssemblyVersion("0.0.14")]
-[assembly: AssemblyFileVersion("0.0.14")]
-[assembly: AssemblyInformationalVersion("0.0.14")]
+[assembly: AssemblyVersion("0.0.15")]
+[assembly: AssemblyFileVersion("0.0.15")]
+[assembly: AssemblyInformationalVersion("0.0.15")]
