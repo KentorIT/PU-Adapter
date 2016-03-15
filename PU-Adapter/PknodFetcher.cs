da8af0a68e5f79b93693554147d13d3621808709
Albin Sunnanbo <albin.sunnanbo@kentor.se>
2016-03-15 18:11:02 +0100

    Refactor: Use HttpWebRequest instead of WebClient to prepare for per request certificate validation

--- PU-Adapter/PknodFetcher.cs
+++ PU-Adapter/PknodFetcher.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
@@ -43,14 +44,24 @@ namespace Kentor.PU_Adapter
             try
             {
                 var requestUrl = new Uri(PknodUrl, (plusString ? "PKNODPLUS" : "PKNOD") + "?arg=" + Uri.EscapeDataString(personnummer));
-                WebClient wc = new WebClient();
+                personnummer = personnummer.Replace("-", "").Replace(" ", "");
+                HttpWebRequest request = HttpWebRequest.CreateHttp(requestUrl);
                 if (!string.IsNullOrEmpty(UserName))
                 {
-                    wc.Credentials = new NetworkCredential(UserName, Password);
+                    request.Credentials = new NetworkCredential(UserName, Password);
                 }
-                personnummer = personnummer.Replace("-", "").Replace(" ", "");
-                var data = wc.DownloadData(requestUrl);
-                var latin1 = Encoding.GetEncoding("ISO-8859-1");
+
+                string data;
+                using (var response = request.GetResponse())
+                {
+                    using (var stream = response.GetResponseStream())
+                    {
+                        using (var sr = new StreamReader(stream, Encoding.GetEncoding("ISO-8859-1")))
+                        {
+                            data = sr.ReadToEnd();
+                        }
+                    }
+                }
 
                 // Data is in the format
                 // -----------------------------------------------------
@@ -60,9 +71,8 @@ namespace Kentor.PU_Adapter
                 //07040000191212121212191212121912121212121TOLVANSSON, TOLVAN TOLVAR STIGEN              12345STOCKHOLM                           00000000000000000000    0000018019200244  200801162008011600000000                                                                                                                                                                                    00000000000000000000        132204  03132204  V[STRA KUNGSHOLMEN            17101648M22V[STRA KUNGSHOLMEN                                STOCKHOLM / EKER\     1734    CENTRALA STOCKHOLMS PSYKIATRIS1329999                                               8                                                              _
                 // -----------------------------------------------------
                 // (between the dashed lines). We need to pick the first long line. Lets say long as > 100 characters
-                var allDataAsText = latin1.GetString(data);
-                var allDataAsRows = allDataAsText.Split(new[] { Environment.NewLine, "\r", "\n" }, StringSplitOptions.RemoveEmptyEntries);
-                return allDataAsRows.Where(l => l.Length >= 100).FirstOrDefault();
+                var dataAsRows = data.Split(new[] { Environment.NewLine, "\r", "\n" }, StringSplitOptions.RemoveEmptyEntries);
+                return dataAsRows.Where(l => l.Length >= 100).FirstOrDefault();
             }
             catch (WebException wex)
             {
