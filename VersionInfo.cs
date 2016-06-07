b95f2733a676318153f506c40cc1cc9a99ad6179
Albin Sunnanbo <albin@sunnanbo.com>
2016-06-07 09:35:52 +0200

    Fix names and spelling in comments

--- VersionInfo.cs
+++ VersionInfo.cs
@@ -4,14 +4,14 @@ using System.Reflection;
 [assembly: AssemblyCopyright("Copyright © Kentor and contributors 2015")]
 
 
-// Kentor.AuthServices uses semantic versioning in three parts
+// Kentor.PU-Adapter uses semantic versioning in three parts
 //
 //      Major Version
 //      Minor Version 
 //      Patch Number
 //
 // An odd patch number is a development version, an even patch number is
-// a relased version.
+// a released version.
 
 [assembly: AssemblyVersion("0.0.31")]
 [assembly: AssemblyFileVersion("0.0.31")]
