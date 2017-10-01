cbf527a45686552ba2fea0c40a23328ca8681078
Albin Sunnanbo <albin.sunnanbo@kentor.se>
2017-10-01 21:17:34 +0200

    Updated version number to 0.0.39 for development.

--- VersionInfo.cs
+++ VersionInfo.cs
@@ -13,6 +13,6 @@ using System.Reflection;
 // An odd patch number is a development version, an even patch number is
 // a released version.
 
-[assembly: AssemblyVersion("0.0.38")]
-[assembly: AssemblyFileVersion("0.0.38")]
-[assembly: AssemblyInformationalVersion("0.0.38")]
+[assembly: AssemblyVersion("0.0.39")]
+[assembly: AssemblyFileVersion("0.0.39")]
+[assembly: AssemblyInformationalVersion("0.0.39")]
