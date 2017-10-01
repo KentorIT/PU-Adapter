bff0611297713d9ce6b86ec18b5014f9c18f8712
Albin Sunnanbo <albin.sunnanbo@kentor.se>
2017-10-01 20:57:46 +0200

    Test for invalid parse check

--- PU-Adapter.Test/FieldDataTest.cs
+++ PU-Adapter.Test/FieldDataTest.cs
@@ -14,5 +14,14 @@ namespace Kentor.PU_Adapter.Test
             var pknodData = new PknodData(CommonData.TolvanPknodResult);
             pknodData.GetStringFieldFromPosition(new FieldDefinitions.FieldDefinition(3, 12)).Should().Be("040000191212");
         }
+
+        [TestMethod]
+        public void GetIntFieldFromPositionShouldThrowSensibleErrorsForNonParsableData()
+        {
+            var pknodData = new PknodData(CommonData.TolvanPknodResult);
+            Action a = () => pknodData.GetIntFieldFromPosition(FieldDefinitions.Pknod.Namn_0042); // Namn is not an int, should throw an error
+
+            a.ShouldThrow<InvalidOperationException>().WithMessage("Could not parse \"TOLVANSSON, TOLVAN\" (position 42) as int");
+        }
     }
 }
